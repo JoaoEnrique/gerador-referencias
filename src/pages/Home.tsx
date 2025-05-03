dc6411165ff070abb21516bf51e40c3b04edbc5b
JoaoEnrique <joaoenriquealves13@gmail.com>
2025-05-03 12:45:05 -0300

    feat: home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,12 @@
+import "./style.css"
+
 export default function Home() {
-    return <h1>Página Inicial</h1>
+    return (
+        <div className="flex flex-col justify-center items-center landing-container">
+            <h1 className="text-5xl">CIRA</h1>
+            <h3 className="text-4xl text-secondary">Gerador de Referências</h3>
+            <button className="user-select-none text-white font-bold py-2 px-4 rounded-full">Gerar</button>
+        </div>
+    );
 }
   
